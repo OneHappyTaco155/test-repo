7c628de7bcfe0bd10db910cbb0229d21eab0959a
Jack <whodareswins12@gmail.com>
2022-03-16 15:03:40 -0400

    More

--- Validator.java
+++ Validator.java
@@ -3,6 +3,7 @@
 public class Validator {
 	
 	public void validateRequest(Request request) {
-		int count = !(car.getColor().equals(CarColor.RED) || car.getColor().equals(CarColor.BLUE) || car.getColor(CarColor.GREEN)) ? car.getPassengers(): 0;
+		CarType carType = car.getType();
+		int count = (Arrays.asList(CarColor.RED.name(), CarColor.GREEN.name(), CarColor.BLUE.name() || carType.equals(CarType.eqauls(CarType.PASSENGER) ? 0 : car.getPassengerCount();
 	}
 
